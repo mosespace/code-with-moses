9d992fd55d79499ff5997ba0a435a17399d0feaf
mosespace <kisakyemoses50@gmail.com>
2023-11-22 00:31:51 +0300

    feat: added footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Image from "next/image";
+import { FiLogIn } from "react-icons/fi";
+import { GoSearch } from "react-icons/go";
+import MyImage from "../public/mosespace.jpg";
+import Link from "next/link";
+
+export default function Footer() {
+  const footerLinks = [
+    {
+      linkTitle: "Contact",
+      linkPath: "/conatct",
+    },
+    {
+      linkTitle: "Terms Of Service",
+      linkPath: "/terms-of-service",
+    },
+    {
+      linkTitle: "Privacy Policy",
+      linkPath: "/privacy-policy",
+    },
+  ];
+  return (
+    <footer className='flex justify-between items-center gap-[8rem] py-[1.4rem] px-8 text-slate-500 text-xs'>
+      <div className='font-semibold tracking-wide items-center flex gap-3'>
+        <h2>© 2023 ENRA d.o.o. All rights reserved.</h2>
+      </div>
+
+      {/* Login button */}
+      <div className='flex items-center gap-4 font-semibold'>
+        {footerLinks.map((link, i) => (
+          <Link key={i} href={link.linkPath}>
+            {link.linkTitle}
+          </Link>
+        ))}
+      </div>
+    </footer>
+  );
+}
