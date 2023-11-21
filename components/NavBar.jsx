1ec95a3d55cbd8e6b22b56db7ba45d9ec68a3c57
mosespace <kisakyemoses50@gmail.com>
2023-11-21 22:19:52 +0300

    feat: changed the svg to react-icon

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { FiLogIn } from "react-icons/fi";
+import { GoSearch } from "react-icons/go";
 import MyImage from "../public/mosespace.jpg";
 
 export default function NavBar() {
@@ -23,29 +24,9 @@ export default function NavBar() {
           type='text'
           placeholder='Search'
         />
-        <svg
-          className='absolute right-3 z-10 cursor-pointer'
-          width={24}
-          height={24}
-          viewBox='0 0 24 24'
-          fill='none'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <path
-            d='M10 17C13.866 17 17 13.866 17 10C17 6.13401 13.866 3 10 3C6.13401 3 3 6.13401 3 10C3 13.866 6.13401 17 10 17Z'
-            stroke='#4B5563'
-            strokeWidth='1.66667'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-          />
-          <path
-            d='M21 21L15 15'
-            stroke='#4B5563'
-            strokeWidth='1.66667'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-          />
-        </svg>
+        <div className='absolute right-3 z-10 cursor-pointer'>
+          <GoSearch className='w-6 h-6 text-gray-600' />
+        </div>
       </div>
 
       {/* Login button */}
