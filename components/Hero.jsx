126288f2898f79ee379d191db77d354a88aff551
mosespace <kisakyemoses50@gmail.com>
2023-11-22 00:32:04 +0300

    feat: hero section for the home page

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Hero() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      Hello World Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Magnam quia aspernatur officiis voluptas. Cupiditate eligendi consequatur
+      animi natus consectetur a?
+    </div>
+  );
 }
