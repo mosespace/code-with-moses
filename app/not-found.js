66f2b2db0a4b547249b2e0c6912ece1933b01a1f
mosespace <kisakyemoses50@gmail.com>
2023-11-21 22:18:24 +0300

    feat: added not-found page

---
+++ app/not-found.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <>
+      <div className='min-h-full w-full flex flex-grow items-center justify-center'>
+        <div className='rounded-lg bg-white p-8 text-center shadow-xl'>
+          <h1 className='mb-4 text-4xl font-bold'>404</h1>
+          <p className='text-gray-600'>
+            Oops! The page you are browsed could not be found 😥.
+          </p>
+          <Link
+            href='/'
+            className='mt-4 inline-block rounded bg-orange-400 px-4 py-2 font-semibold text-white hover:bg-orange-500'
+          >
+            {" "}
+            Return Back To Home{" "}
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
