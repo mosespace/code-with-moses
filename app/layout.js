2b1fcfc412cf9146328ac75765dbe7f3c17ea26d
mosespace <kisakyemoses50@gmail.com>
2023-11-22 12:24:13 +0300

    feat: changed layout for sidebar

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
 import "../Styles/globals.css";
 import { Raleway } from "next/font/google";
 import NavBar from "../Components/NavBar";
-import SideBar from "../components/SideBar";
 import Footer from "../Components/Footer";
+import SideBar from "../components/SideBar";
+import { SideBarProvider } from "../Context/Context";
 
 const raleway = Raleway({ subsets: ["latin"] });
 
@@ -14,22 +15,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={raleway.className}>
-        <NavBar />
-        <div className='flex h-screen w-screen overflow-x-hidden'>
-          <div className='flex w-full'>
-            <div className='fixed top-0 bottom-0 left-0 overflow-y-auto no-scrollbar right-0 pt-20 w-[20%] border-r border-slate-300 bg-white py-[1.1rem] px-5 text-black'>
-              <SideBar />
-            </div>
-            <div className='bg-slate-50 pt-[4.5rem] ml-[20%] w-full'>
-              {children}
-              <div className='bg-white absolute right-0 left-0 bottom-0 ml-[20%]'>
-                <Footer />
+      <SideBarProvider>
+        <body className={raleway.className}>
+          <NavBar />
+          <div className='flex h-screen w-screen overflow-x-hidden'>
+            <div className='flex w-full'>
+              <div className=''>
+                <SideBar />
               </div>
+              {/* <div className='bg-slate-50 pt-[4.5rem] ml-[20%] w-full'>
+                {children}
+                <div className='bg-white absolute right-0 left-0 bottom-0 ml-[20%]'>
+                  <Footer />
+                </div>
+              </div> */}
             </div>
           </div>
-        </div>
-      </body>
+        </body>
+      </SideBarProvider>
     </html>
   );
 }
