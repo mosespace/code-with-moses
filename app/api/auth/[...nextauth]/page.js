687ac3cc9242e6fb2e0054a90e37d4ae6a0f25eb
mosespace <kisakyemoses50@gmail.com>
2023-11-27 10:06:37 +0300

    feat: initial commit

--- app/api/auth/[...nextauth]/page.js
+++ app/api/auth/[...nextauth]/page.js
@@ -2,9 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/google";
 
-
 export const authOptions = {
-  adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
