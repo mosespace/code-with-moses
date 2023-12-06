6dada8b8c06ac3226dc0f0a940db71a1a0b7b85a
mosespace <kisakyemoses50@gmail.com>
2023-12-06 09:25:52 +0300

    feat: removed error handling on signIn

--- app/components/Auth.jsx
+++ app/components/Auth.jsx
@@ -2,11 +2,10 @@
 import Link from "next/link";
 import Image from "next/image";
 import { signIn } from "next-auth/react";
-import { FaGithub, FaGoogle } from "react-icons/fa";
-import { useRouter } from "next/navigation";
 import { IoCloseSharp } from "react-icons/io5";
 import toast, { Toaster } from "react-hot-toast";
 import MyImage from "../../public/mosespace.jpg";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
 export default function Auth({ toggleSignInOff }) {
   return (
@@ -58,16 +57,15 @@ export default function Auth({ toggleSignInOff }) {
                       } catch (error) {
                         // Handle errors here
                         console.error("Sign-in error:", error);
-
-                        // Show error toast
-                        toast.error("Error when logging in");
                       }
                     }}
                     className='group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-blue-400'
                   >
                     <div className='relative flex items-center space-x-4 justify-center'>
                       <FaGithub className='absolute left-0 w-5 h-5 text-white' />
-                      <span className='block w-max font-semibold tracking-wide text-sm transition text-white duration-300 group-hover:text-blue-600 sm:text-base'></span>
+                      <span className='block w-max font-semibold tracking-wide text-sm transition text-white duration-300 group-hover:text-blue-600 sm:text-base'>
+                        Continue with Github
+                      </span>
                     </div>
                   </button>
                   <button
@@ -89,9 +87,6 @@ export default function Auth({ toggleSignInOff }) {
                       } catch (error) {
                         // Handle errors here
                         console.error("Sign-in error:", error);
-
-                        // Show error toast
-                        toast.error("Error when logging in");
                       }
                     }}
                     className='group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-blue-400'
