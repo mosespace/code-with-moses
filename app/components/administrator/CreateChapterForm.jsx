6680e97d9c54e49756de5709611a1af6cd62c55c
mosespace <kisakyemoses50@gmail.com>
2023-12-06 11:32:47 +0300

    feat: initial commit

--- app/components/administrator/CreateChapterForm.jsx
+++ app/components/administrator/CreateChapterForm.jsx
@@ -143,36 +143,20 @@ export default function CreateChapterForm({
           </label>
           <div className='mt-2'>
             <select
-              defaultValue={id ? course.courseId : ""}
+              // defaultValue={id ? course.courseId : ""}
+              defaultValue={""}
               {...register("courseId")}
               id='course'
               name='course'
               autoComplete='course-name'
-              className='block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6'
+              className='block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-amber-500 sm:max-w-xs sm:text-sm sm:leading-6'
             >
               {/* <option value={currentCourse.id}>{currentCourse.title}</option> */}
-              <option>Canada</option>
-              <option>Mexico</option>
+              <option>Trello Clone</option>
+              <option>Trello Clone</option>
+              <option>Trello Clone</option>
             </select>
           </div>
-        </div>
-        {/* Is Free */}
-        <div className='w-full'>
-          <p className='block text-sm font-medium leading-6 text-gray-900 mb-3'>
-            Chapter Access
-          </p>
-          <label className='relative inline-flex items-center cursor-pointer'>
-            <input
-              {...register("isPaid")}
-              type='checkbox'
-              defaultValue={id ? course.isPaid : true}
-              className='sr-only peer'
-            />
-            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-600"></div>
-            <span className='ml-3 text-sm font-medium text-gray-900 dark:text-gray-300'>
-              {isPaid ? "Paid" : "Free"}
-            </span>
-          </label>
         </div>
 
         {/* Chapter Description */}
