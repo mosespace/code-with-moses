cecbfab72a07622c28d61278f814c09db162d5a1
mosespace <kisakyemoses50@gmail.com>
2023-12-06 11:02:13 +0300

    feat: turned it to be a component

--- app/components/courses/course/Video.jsx
+++ app/components/courses/course/Video.jsx
@@ -6,7 +6,7 @@ import "@vidstack/react/player/styles/default/theme.css";
 import { MediaPlayer, MediaProvider } from "@vidstack/react";
 import "@vidstack/react/player/styles/default/layouts/video.css";
 
-export default function Video() {
+export default function Video({src}) {
   return (
     <div className='py-1 flex flex-col gap-2 text-sm font-semibold player-wrapper'>
       <MediaPlayer
