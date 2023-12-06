863f71b6b25927793c890c239e4b3a158ab83ba3
mosespace <kisakyemoses50@gmail.com>
2023-12-06 11:44:03 +0300

    feat: added react toaster

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "../Styles/globals.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import { Toaster } from "react-hot-toast";
 import { Raleway } from "next/font/google";
 import SideBar from "./components/SideBar";
 import { SideBarProvider } from "../Context/Context";
@@ -15,20 +16,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <SideBarProvider>
-        <body className={raleway.className}>
-          <NavBar />
-          <div className='flex flex-grow h-screen w-screen overflow-x-hidden'>
-            <div className='flex w-full'>
-              <div>
-                <SideBar />
-              </div>
-              <div className='pt-[4.5rem] lg:ml-[20%] flex-grow w-full min-h-full'>
-                {children}
+        <Toaster>
+          <body className={raleway.className}>
+            <NavBar />
+            <div className='flex flex-grow h-screen w-screen overflow-x-hidden'>
+              <div className='flex w-full'>
+                <div>
+                  <SideBar />
+                </div>
+                <div className='pt-[4.5rem] lg:ml-[20%] flex-grow w-full min-h-full'>
+                  {children}
+                </div>
               </div>
             </div>
-          </div>
-          <Footer />
-        </body>
+            <Footer />
+          </body>
+        </Toaster>
       </SideBarProvider>
     </html>
   );
