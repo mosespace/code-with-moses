4df58d07b1558ebc5c1b510d515e0c8bee104664
mosespace <kisakyemoses50@gmail.com>
2023-12-06 18:21:44 +0300

    feat: got course by slug

--- app/course/[id]/page.js
+++ app/course/[id]/page.js
@@ -1,17 +1,20 @@
-// import CreateCourseForm from "@/app/components/administrator/CreateCourseForm";
-import CreateForm from "@/app/components/administrator/CreateForm";
 import React from "react";
+import { getData } from "@/utils/getData";
+import CreateForm from "@/app/components/administrator/CreateForm";
 
-export default function page({ params: { id } }) {
+export default async function page({ params: { id } }) {
+  const courses = await getData("courses");
+  const slug = courses.map((course) => {
+    return course.slug;
+  });
+
+  const courseData = await getData(`courses/${slug}`);
+  // console.log(slug);
+  // console.log(courseData);
   return (
     <div className='lg:py-8 lg:px-4 px-4 py-10 flex items-center justify-center gap-4'>
-      {/* <div>
-        <span className='text-2xl font-bold'>
-          Key Hints On Creating the Courses, Chapter and The Attachments
-        </span>
-      </div> */}
       <div className=''>
-        <CreateForm className='w-2/4' />
+        <CreateForm className='w-2/4' courses={courses} course={courseData} />
       </div>
     </div>
   );
