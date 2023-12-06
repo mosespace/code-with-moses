08ae435c8d2d84f271abc23772db5c828b613f79
mosespace <kisakyemoses50@gmail.com>
2023-12-06 10:04:43 +0300

    feat: made just small changes to the code

--- app/course/[id]/page.js
+++ app/course/[id]/page.js
@@ -3,16 +3,15 @@ import CreateForm from "@/app/components/administrator/CreateForm";
 import React from "react";
 
 export default function page({ params: { id } }) {
-
   return (
-    <div className='lg:py-8 lg:px-4 px-4 py-10 flex items-start justify-center gap-4'>
+    <div className='lg:py-8 lg:px-4 px-4 py-10 flex items-center justify-center gap-4'>
       {/* <div>
         <span className='text-2xl font-bold'>
           Key Hints On Creating the Courses, Chapter and The Attachments
         </span>
       </div> */}
       <div className=''>
-        <CreateForm className='w-full' />
+        <CreateForm className='w-2/4' />
       </div>
     </div>
   );
