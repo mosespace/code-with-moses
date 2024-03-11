b1717056210fb26c534c816c232cbc66855932ce
mosespace <kisakyemoses50@gmail.com>
2024-03-11 16:25:07 +0300

    fix: fixing development bug
    `Module not found: Can't resolve '@/app/components/student/CourseProgress'
    `

--- app/course/[id]/page.jsx
+++ app/course/[id]/page.jsx
@@ -1,10 +1,11 @@
-import CourseProgress from "@/app/components/student/CourseProgress";
+// import CourseProgress from "@/app/components/student/CourseProgress";
 import React from "react";
 
-export default function page({params:{id}}) {
+export default function page({ params: { id } }) {
   return (
     <div className='lg:py-8 lg:px-4 px-4'>
-      <CourseProgress />
+      {/* <CourseProgress /> */}
+      Lorem Inspum
     </div>
   );
 }
